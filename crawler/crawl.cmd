c213a7b524b34926cd96831f284de9404f4ed884
Robbie <robbie.jiao@gmail.com>
2017-06-18 22:51:12 -0700

    Added crawl.cmd for windows

---
+++ crawler/crawl.cmd
@@ -0,0 +1,24 @@
+if "%1"=="" (
+    ECHO "Usage: %0 <market name> [database output directory]"
+    ECHO "   market name:"
+    ECHO "       appchina.com"
+    ECHO "       hiapk.com"
+    ECHO "       anzhi.com"
+    ECHO "       android.d.cn"
+    ECHO "       mumayi.com "
+    ECHO "       gfan.com"
+    ECHO "       nduoa.com"
+    ECHO "       3gyu.com"
+    ECHO "       angeeks.com"
+    ECHO "       appfun.cn"
+    ECHO "       jimi168.com"
+    ECHO "   database output directory:"
+    ECHO "       default: ../repo/databases/"
+    exit 2
+    )
+
+if "%2"=="" (
+    scrapy crawl android_apps_spider -s JOBDIR=job_%1 -a market=%1
+    ) else (
+    scrapy crawl android_apps_spider -s JOBDIR=job_%1 -a market=%1 -a database_dir=%2
+    )
